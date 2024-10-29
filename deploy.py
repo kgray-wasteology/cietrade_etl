64cd7ee92f6ba22270c380b33f57a25385e20573
kgray-wasteology <kgray@wasteologygroup.com>
2024-10-29 08:34:15 -0400

    Deploying via github

--- deploy.py
+++ deploy.py
@@ -1,21 +1,8 @@
-# deploy.py
 from prefect import flow
-from datetime import timedelta
-from prefect_github import GitHubRepository, GitHubCredentials
-
+from accounts_flow import accounts_update_flow
 
 if __name__ == "__main__":
-    github_token = os.getenv("GITHUB_TOKEN")
-    github_url = f"https://{github_token}@github.com/kgray-wasteology/cietrade_etl"
-
-    # Create deployment from source
-    flow.from_source(
-        source=github_url,
-        entrypoint="flows/accounts_flow.py:accounts_update_flow",  # path to flow file and function
-    ).deploy(
-        name="accounts-update-github",
-        work_pool_name="cietrade-process-pool",
-        interval=timedelta(hours=24),  # Optional: Set schedule
-        parameters={},  # Add any default parameters if needed
-        job_variables={"env": {"EXTRA_PIP_PACKAGES": "prefect-github psycopg2-binary"}},
-    )
+    accounts_update_flow.from_source(
+        source="https://github.com/kgray-wasteology/cietrade_etl",
+        entrypoint="accounts_flow.py:accounts_update_flow",
+    ).deploy(name="accounts-update-flow", work_pool_name="cietrade-process-pool")
