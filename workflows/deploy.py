376a5c05343a602f0f88ba4e7a064e29e1a0ceb6
kgray-wasteology <kgray@wasteologygroup.com>
2024-10-29 14:46:50 -0400

    Update GitHub Actions workflow with block creation

--- workflows/deploy.py
+++ workflows/deploy.py
@@ -1,8 +1,16 @@
 from prefect import flow
+from prefect_github import GitHubRepository
 from accounts_flow import accounts_update_flow
+from datetime import timedelta
 
 if __name__ == "__main__":
+    # Load the configured GitHub repo block
+    github_repo = GitHubRepository.load("cietrade-etl-repo")
+
     accounts_update_flow.from_source(
-        source="https://github.com/kgray-wasteology/cietrade_etl",
-        entrypoint="accounts_flow.py:accounts_update_flow",
-    ).deploy(name="accounts-update-flow", work_pool_name="cietrade-process-pool")
+        source=github_repo, entrypoint="workflows/accounts_flow.py:accounts_update_flow"
+    ).deploy(
+        name="accounts-update-flow",
+        work_pool_name="cietrade-process-pool",
+        interval=timedelta(hours=24),
+    )
